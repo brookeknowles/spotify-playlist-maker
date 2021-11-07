e7895e7deb87a277539d0fa6b36dc181e1e311ae
Brooke Knowles <62309663+brookeknowles@users.noreply.github.com>
2021-11-07 20:27:08 +1300

    Update api_keys_tokens.py

--- api_keys_tokens.py
+++ api_keys_tokens.py
@@ -1,9 +1,9 @@
 def spotify_user_id():
-    return '1257120559'
+    return '' # get spotify user ID from desktop site. getting rid of so its not publicly avail on github lol
 
 
 def spotify_token():
-    return 'BQC1EZPU0jKcRdKBFv1JOnKcmzKmeGcERGr_cO7ouGoHizH5Dra4Soup0pd_tzA5UsBa4d4Zl0_3w5Q912D9t5hDBFxxrz1TFaXkqXjLb0lm-tqj2y8oT3dCgvcqLmAPt8dpBg4wiAskyg6ltugYUEc0orHitVFUBipJiHvVQ6afyX8'
+    return '' # developers.spotify.com playlist token
 
 def last_fm_api_key():
-    return 'a2e784b5b1fc5e3a9b11b134c7303959'
+    return '' # from last fm api website
